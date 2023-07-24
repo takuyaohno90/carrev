e8b33b0e7e04c0c80f5fb0cc8e0fe0cc87e99cb8
takuyaohno90 <131519948+takuyaohno90@users.noreply.github.com>
2023-07-24 15:36:59 +0000

    1車種登録途中まで

---
+++ app/controllers/admin/caritems_controller.rb
@@ -0,0 +1,21 @@
+class Admin::CaritemsController < ApplicationController
+  def check
+  end
+  
+  def new
+    @caritem = CarItem.new
+  end
+  
+  def create
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+end
