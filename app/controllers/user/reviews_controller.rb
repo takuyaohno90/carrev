dbc67c53ee02488c55844d534256e1ec33dedb14
takuyaohno90 <131519948+takuyaohno90@users.noreply.github.com>
2023-08-08 14:28:12 +0000

    レビュー投稿機能途中

---
+++ app/controllers/user/reviews_controller.rb
@@ -0,0 +1,25 @@
+class User::ReviewsController < ApplicationController
+  def search
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  def index
+  end
+end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :car_item
+  belongs_to :user
+  has_one_attached :image
 end
