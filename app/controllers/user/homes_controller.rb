e78c023168f9cbd5e260a29efbd4cdb04f59ee1e
takuyaohno90 <131519948+takuyaohno90@users.noreply.github.com>
2023-09-09 18:10:31 +0000

    レイアウト30まで

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,7 +1,9 @@
 class User::HomesController < ApplicationController
   def top
+    @car_items =  CarItem.order(created_at: :desc).limit(6)
+    @sixth_car_item = @car_items[5]
   end
-  
+
   def about
   end
 end
